d3dd706de6990e75eb7c7035fac289e9ed6b7d84
Tsunami014 <tsunami014@duck.com>
2026-02-02 08:35:53 +1100

    Added the y/n demo from kudos

--- src/boot.rs
+++ src/boot.rs
@@ -1,4 +1,28 @@
+use kudos::utils::fancy;
+use kudos::utils::keys::choice;
+use kudos::task::{Task, executor::Executor, keyboard::ScancodeStream};
+use kudos::{print, println, printlgln, LogType};
+
+async fn main() {
+    let mut scancodes = ScancodeStream::new();
+    loop {
+        let chararr = ['y', 'n'];
+        let c = choice(&mut scancodes, &chararr).await;
+        fancy::clear_line();
+        if c == 'y' {
+            print!("Yes!");
+        } else {
+            print!("No.");
+        }
+    }
+}
+
 /// This function will run when running the main program
 pub fn on_boot() {
-    kudos::printlgln!("Hello world!");
+    printlgln!("Hello world!");
+    println!("Press y/n");
+
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(main()));
+    executor.run();
 }
