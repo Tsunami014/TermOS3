9fd9d9d896827246555b7f84be1eda8eebc11a78
Tsunami014 <tsunami014@duck.com>
2026-02-04 16:12:39 +1100

    Added alignment to the label!

--- src/winapi/components.rs
+++ src/winapi/components.rs
@@ -1,4 +1,4 @@
-use crate::winapi::buffer::Writer;
+use crate::winapi::buffer::{Writer, WINDOW_WIDTH};
 use crate::println_at;
 
 extern crate alloc;
@@ -12,23 +12,44 @@ pub trait Element {
 }
 //print_at!(self.core.writer().lock(), "{}", c);
 
+#[allow(dead_code)]
+pub enum Alignment {
+    Left = 0,
+    Middle = 1,
+    Right = 2,
+}
+
 pub struct Label {
-    text: String
+    text: String,
+    pub align: Alignment
 }
 #[allow(dead_code)]
 impl Label {
     pub fn new() -> Self {
-        Self { text: "".to_string() }
+        Self { text: "".to_string(), align: Alignment::Middle }
     }
     pub fn new_str(txt: &str) -> Self {
-        Self { text: txt.to_string() }
+        Self { text: txt.to_string(), align: Alignment::Middle }
     }
     pub fn new_string(txt: String) -> Self {
-        Self { text: txt }
+        Self { text: txt, align: Alignment::Middle }
     }
 }
 impl Element for Label {
     fn redraw(&self, w: &mut spin::MutexGuard<'_, Writer>) {
-        println_at!(w, "{}", self.text)
+        let bytes = self.text.as_bytes();
+        for i in (0..bytes.len()).step_by(WINDOW_WIDTH) {
+            let end = (i + WINDOW_WIDTH).min(bytes.len());
+            let part = &self.text[i..end];
+
+            let len = end - i;
+            let spaces = match self.align {
+                Alignment::Left => 0,
+                Alignment::Middle => (WINDOW_WIDTH - len) / 2,
+                Alignment::Right => WINDOW_WIDTH - len,
+            };
+
+            println_at!(w, "{}{}", " ".repeat(spaces), part)
+        }
     }
 }
